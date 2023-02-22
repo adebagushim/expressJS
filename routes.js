c6a60ecf303cca9e5d0ad7036a19b4022ae405cc
adebagus <adebagushim333@gmail.com>
2023-02-22 20:52:51 +0700

    ?

--- routes.js
+++ routes.js
@@ -7,8 +7,22 @@ const path = require('path');
 router.get('/', (req, res) => {
     res.json({
         Status: 'BERHASIL',
-        About: 'Mencoba Express JS',
-        Pilihan: 'daftarmenu/makanan atau daftarmenu/minuman'
+        About: 'Mencoba Express JS',        
+    })
+});
+
+router.post('/products/', (req, res) => {
+
+    const {products} = req.body;
+    const {description} = req.body;
+    const {price} = req.body;
+    const {stock} = req.body;
+
+    res.send({
+        Products: `${products}`,
+        Description: `${description}`,
+        Price: `${price}`,
+        Stock: `${stock}`,
     })
 });
 
@@ -25,39 +39,50 @@ router.get('/daftarmenu/:name', (req, res) => {
             Menu_2: 'Jus',
             Menu_3: 'Water'
         });
-    } else {
+    }
+    else {
         res.status(404);
         res.send({
             status: 'FAILED',
             message: 'Resource ' + req.originalUrl + 'Not Found'
     })
     }
-    
 });
 
-router.post('/coba/',upload.single('image'), (req, res) => {
-    const {name, price ,stock, status} = req.body;
-    const image = req.file;
-    if(image) {
-        const target = path.join(__dirname, 'uploads', image.originalname);
-        fs.renameSync(image.path, target)
-        res.json({
-            name,
-            price,
-            stock,
-            status,
-            image
-        });
-        send.file(target);
-    }
-});
+router.post('/menubaru/', (req, res) => {
 
-router.get('/:category/:tag', (req, res) => {
-    const {category, tag} = req.params;
+    const {menu} = req.body;
+    const {harga} = req.body;
+
     res.send({
-        category, tag,
-    });
+        Menu_Baru: `${menu} Rp. ${harga}`,
+    })
 });
+
+
+// router.post('/coba/',upload.single('image'), (req, res) => {
+//     const {name, price ,stock, status} = req.body;
+//     const image = req.file;
+//     if(image) {
+//         const target = path.join(__dirname, 'uploads', image.originalname);
+//         fs.renameSync(image.path, target)
+//         res.json({
+//             name,
+//             price,
+//             stock,
+//             status,
+//             image
+//         });
+//         send.file(target);
+//     }
+// });
+
+// router.get('/:category/:tag', (req, res) => {
+//     const {category, tag} = req.params;
+//     res.send({
+//         category, tag,
+//     });
+// });
 
 
 module.exports = router;
